e7dc3d55b0b87bd61b8d26c017256f1eb2832831
Arnav-006 <mc240041004@iiti.ac.in>
2025-10-23 12:25:16 +0530

    File containing historical data

--- data_extract.py
+++ data_extract.py
@@ -6,6 +6,8 @@ password = 'Arnav@TradingView7'
 
 tv = TvDatafeed(username, password)
 
-tcs_data = tv.get_hist(symbol='TCS',exchange='NSE',interval=Interval.in_1_hour,n_bars=1000)
+tcs_data = tv.get_hist(symbol='TCS',exchange='NSE',interval=Interval.in_1_hour,n_bars=10000)
 print(tcs_data.head())
 print(tcs_data.tail())
+
+tcs_data.to_csv('TCS_OHLCV_1H.csv')
